d9bfe1ec4c0160428e2c693b8c9e9ef9d7abe63c
darshan-gowdaa <darshangowdaa223@gmail.com>
2025-11-14 07:36:12 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import NewsComp from "./components/NewsComp";
 
